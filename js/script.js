e4cb2eebec03b995213df006479e6ce315af7306
Leograzioli <leograzioli@gmail.com>
2022-10-07 15:57:28 +0200

    ticket con css

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ const userAgeInput = document.getElementById("user-age");
 const submitBtn = document.getElementById("submit");
 const kmPrice = 0.21;
 
+//random numer
+const rndNumber = Math.floor(Math.random() * (10000 - 99999) ) + 99999;
+
 
 //on click 
 submitBtn.addEventListener("click",  function () {
     //input value
     const userName = userNameInput.value;
+    console.log(userName)
     const userKm =  parseInt(userKmInput.value); 
     const userAge = userAgeInput.value;
 
@@ -29,8 +33,16 @@ submitBtn.addEventListener("click",  function () {
         console.log(ticketPrice);
     };
 
-    ticketPrice - ticketPrice.toFixed(2)
+    const finalPrice = ticketPrice.toFixed(2)
 
-    document.getElementById("result").innerHTML = `${ticketPrice} euros.`
+    document.getElementById("result").innerHTML = `${finalPrice} euros.`
+    document.getElementById("passager-name").innerHTML = userName;
+    document.getElementById("ticket-distance").innerHTML += `${userKm} KM`;
+    document.getElementById("offer-type").innerHTML = userAge;
+    document.getElementById("ticket-number").innerHTML = rndNumber;
+
+    userNameInput.value = "";
+    userAgeInput.value = "";
+    userKmInput.value = "";
 });
 
